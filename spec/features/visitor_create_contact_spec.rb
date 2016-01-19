a24c75aa589f931b07f7f582317ebfce9910f44b
tasilio <tgb@inbox.com>
2016-01-19 16:21:27 +0300

    Update capybara contacts create scenario

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -6,4 +6,12 @@ feature "Contact Creation" do
 		expect(page).to have_content I18n.t('contacts.contact_us')
 
 	end
+	scenario "allows a guest to create message in contacts form" do
+		visit '/contacts'
+		fill_in :contact_email, :with => 'art@art.ru'
+		fill_in :contact_message, :with => 'asdsadsad'
+		click_button 'Send message'
+
+		expect(page).to have_content 'Thanks'
+	end
 end
