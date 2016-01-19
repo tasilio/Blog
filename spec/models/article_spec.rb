53c9177f39d6e1b0db1c620986f934ee4da6532e
tasilio <tgb@inbox.com>
2016-01-19 12:23:03 +0300

    Add spec for lenght validation in article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,8 +2,10 @@ require "spec_helper"
 
 describe Article do  
 	describe "validation" do # Проверяем работу валидации модели
-		it  {should validate_presence_of :title}
-		it 	{should validate_presence_of :text}
+		it {should validate_presence_of :title}
+		it {should validate_presence_of :text}
+		it {should validate_legth_of :title}
+		it {should validate_legth_of :text}
 	end
 	describe "associations" do # Проверяем работу связи 1 к множеству модели
 		it  {should have_many :comments}
