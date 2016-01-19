16dcd0e6a0045acf70683f9fb181fb80a100f1dc
tasilio <tgb@inbox.com>
2016-01-19 15:59:52 +0300

    Bug fixing

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 feature "Contact Creation" do
 	scenario "allows acces to contacts page" do
 		visit '/contacts'
-		expect(page).to have_content 'Жопа'
+		expect(page).to have_content I18n.t('contacts.contact_us')
 
 	end
 end
