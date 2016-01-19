f51978cc207b0277d8ef0c382eb85d1ef9d49ecc
tasilio <tgb@inbox.com>
2016-01-19 10:08:25 +0300

    Add spec for validation in article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
-describe Article do
-	it  {should have_many :comments}
+describe Article do  
+	describe "validation" do # Проверяем работу валидации модели
+		it  {should validate_presence_of :title}
+		it 	{should validate_presence_of :text}
+	end
+	describe "associations" do # Проверяем работу связи 1 к множеству модели
+		it  {should have_many :comments}
+	end
+
 end
