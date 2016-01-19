246fe235905091403b714b154a0148683c20ab74
tasilio <tgb@inbox.com>
2016-01-19 10:50:49 +0300

    Add factroy girl test for proc subject in article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,5 +8,13 @@ describe Article do
 	describe "associations" do # Проверяем работу связи 1 к множеству модели
 		it  {should have_many :comments}
 	end
+	describe "#subject" do
+		it "returns the article title" do
+			# Создаем объект артикле хитрым способом
+			article = create(:article, title: 'Lorem ipsum')
+			# assert, проверка
+			expect(article.subject).to eq 'Lorem ipsum'
+		end
+	end
 
 end
