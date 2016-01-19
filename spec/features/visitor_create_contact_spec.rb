8034f72c157e6238b915f1922f5fe5257c3cffc0
tasilio <tgb@inbox.com>
2016-01-19 15:06:37 +0300

    Add capybara for contacts

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 feature "Contact Creation" do
 	scenario "allows acces to contacts page" do
-		visit '/contacts/new'
-		expect(page).to have_content 'Blabla'
+		visit '/contacts'
+		expect(page).to have_content 'Жопа'
 
 	end
 end
